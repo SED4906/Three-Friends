122ce3820623230fac2414914adf03df938c8730
rotom <ron.benaroya@digipen.edu>
2023-06-08 13:20:40 -0700

    Stopwatch timer

---
+++ stopwatch_label.gd
@@ -0,0 +1,15 @@
+extends Label
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	text = "Time: " + str(stepify(get_node("/root/Stopwatch").time_taken, 0.01)) +"s"
